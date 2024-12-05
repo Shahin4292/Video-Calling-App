411bbb76a3f1504189ed20ad39524312814f017c
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-05 21:00:48 +0600

    app Id or token add done

---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+const appId = 'eedd8819da7045c599ee8f4b18e3bba6';
+const appCertification = 'bf4aa8d6da0a468b9fcfaccac6603078';
+const channelId = '1by1or1by2channel';
+const appToken =
+    '007eJxTYHjGPO/EBBv+O8L3pJ2u+kzl/rYs2PLJ07BND+a2zO6y+LxegSE1NSXFwsLQMiXR3MDENNnU0jI11SLNJMnQItU4KSnR7Oq+wPSGQEaG43fzGBihEMQXZDBMqjTMLwKSRskZiXl5qTkMDADPDyb/';
