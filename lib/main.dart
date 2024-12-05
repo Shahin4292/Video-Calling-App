2afdce267f4e55110f482c5a259a7d363b67e85b
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-05 20:51:18 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:video_calling_app/pages/index_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: IndexPage(),
     );
   }
 }
